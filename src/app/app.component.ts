a69517daa9e31887b78043fc5983d54788d9bfe1
Denis Bokov <denisbokov1703@gmail.com>
2023-10-27 18:15:29 +0200

    start todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+const todos = [
+  { id: 1, title: 'HTML + CSS', completed: true },
+  { id: 2, title: 'JS + TS', completed: false },
+  { id: 3, title: 'React', completed: false },
+  { id: 4, title: 'Angular', completed: false },
+];
+
+console.log(todos);
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +16,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app--angular';
+  todos = todos;
 }
